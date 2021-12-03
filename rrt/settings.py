ae803ecd2fe2b391582fa987d0284fa2e2660b5d
Been Hodges <benjaminshodges@gmail.com>
2021-12-02 22:32:55 -0500

    added hosting settings

--- rrt/settings.py
+++ rrt/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -191,3 +192,4 @@ CORS_ORIGIN_WHITELIST = (
     'https://been-h.github.io'
 ) 
 
+django_heroku.settings(locals())
