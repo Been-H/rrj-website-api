c79bc69ed215b889f4f58151c4f2e162c227f45b
Ben Hodges <71901668+Been-H@users.noreply.github.com>
2023-03-17 17:15:24 -0400

    Update settings.py

--- rrt/settings.py
+++ rrt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a2+0bvivrm(m5t2!3h+b#!#(z(zdru@=!xh*u&8=e(-8kkuoe6'
+SECRET_KEY = '###############################################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
